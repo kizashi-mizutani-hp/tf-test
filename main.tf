84c70278d37e2addd8c7aa6aec4fd075bff22b72
Kizashi Mizutani <mizutani@heptagon.co.jp>
2024-10-14 15:55:39 +0900

    フォルダの変更
    serverからresourceに変更

--- main.tf
+++ main.tf
@@ -3,17 +3,17 @@ module "network" {
   source = "./network"
 }
 
-module "server" {
-  source = "./server"
+module "resource" {
+  source = "./resource"
 
-  # networkモジュールのoutput.tfで定義された出力をserverモジュールに渡す
+  # networkモジュールのoutput.tfで定義された出力をresourceモジュールに渡す
   main_vpc          = module.network.main_vpc
   public_subnet_1a  = module.network.public_subnet_1a
   public_subnet_1c  = module.network.public_subnet_1c
   private_subnet_1a = module.network.private_subnet_1a
   private_subnet_1c = module.network.private_subnet_1c
 
-  # セキュリティグループのIDをserverモジュールに渡す
+  # セキュリティグループのIDをresourceモジュールに渡す
   ec2_sg            = [module.network.security_group_ec2_sg]
   bastion_sg        = [module.network.security_group_bastion_sg]
   alb_sg            = [module.network.security_group_alb_sg]
